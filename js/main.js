63cce71ab400c1218a4fe84f5fe0310a9dae3e80
Михаил Калмыков <68296867+Mike-Vazovskiy@users.noreply.github.com>
2023-07-31 16:31:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const apiUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+
+let imgBox = document.getElementById("imgBox")
+let qrImage = document.getElementById("qrImage")
+let qrText = document.getElementById("qrText")
+
+async function generateQR() {
+    if(qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    } else {
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error")
+        }, 1000)
+    }
+}
+
+qrText.addEventListener("keypress", function(e){
+    if(e.which === 13){
+        generateQR()
+    }
+})
